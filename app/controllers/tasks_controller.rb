aa6ed0399370f55bf83e8f55dcca0688d7abdc46
lui.alber <the.twilight.dance.princes@gmail.com>
2020-04-12 07:07:08 +0000

    second commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,35 +4,35 @@ class TasksController < ApplicationController
   end
 
   def show
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def new
-      @tasks = Task.new
+      @task = Task.new
   end
 
   def create
-      @tasks = Task.new(task_params)
+      @task = Task.new(task_params)
 
-    if @tasks.save
-      flash[:success] = 'task が正常に投稿されました'
-      redirect_to @tasks
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'task が投稿されませんでした'
+      flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
   end
 
   def edit
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def update
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
 
-    if @tasks.update(task_params)
+    if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'task は更新されませんでした'
       render :edit
@@ -40,10 +40,10 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @tasks = Task.find(params[:id])
-    @tasks.destroy
+      @task = Task.find(params[:id])
+    @task.destroy
 
-    flash[:success] = 'task は正常に削除されました'
+    flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
 end

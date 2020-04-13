2b74e2ce6662fbec82592374bd2d885d97e56730
lui.alber <the.twilight.dance.princes@gmail.com>
2020-04-13 03:07:46 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_132821) do
+ActiveRecord::Schema.define(version: 2020_04_12_225213) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
